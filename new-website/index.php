61af6cba8710a4c62e371958f178b53eab70c4f5
MadCaper <59817666+MadCaper@users.noreply.github.com>
2021-05-23 23:32:05 -0300

    Index page preparation
    Changed file type and global nav inclusion

--- new-website/index.php
+++ new-website/index.php
@@ -19,51 +19,8 @@
                 <span class="iconify" data-icon="bi:list" data-inline="false"></span>
             </button>
         </div>
-        <nav id="left-navbar">
-            <ul>
-                <li>
-                    <a href="index.html" class="nav-item-home active">Home</a>
-                </li>
-                <li>
-                    <a href="about.html" class="nav-item-roadmap">About</a>
-                </li>
-                <li>
-                    <a href="https://raptr.finance/staking" class="nav-item-staking">Staking</a>
-                </li>
-                <!--<li>
-                    <a href="lottery.html" class="nav-item-lottery">Lottery</a>
-                </li>-->
-                <li>
-                    <a href="faq.html" class="nav-item-faq">FAQ</a>
-                </li>
-<!--                <li>-->
-<!--                    <a href="news.html" class="nav-item-news">News</a>-->
-<!--                </li>-->
-            </ul>
 
-
-            <div class="navigation-footer">
-                <div class="text-center text-lg-left mb-2">
-                    <a href="https://twitter.com/raptor_token" class="btn-social">
-                        <span class="iconify" data-icon="jam:twitter-circle" data-inline="false"></span>
-                    </a>
-                    <a href="https://t.me/PhilosoRaptorToken" class="btn-social">
-                        <span class="iconify" data-icon="ri:telegram-line" data-inline="false"></span>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xf9A3FdA781c94942760860fc731c24301c83830A" class="btn btn-success btn-block">
-                        Buy on PancakeSwap
-                    </a>
-                </div>
-                <div class="mt-3">
-                    <a href="https://bscscan.com/token/0xf9a3fda781c94942760860fc731c24301c83830a#balances" class="btn btn-light btn-block">
-                        View on BSCscan
-                    </a>
-                </div>
-            </div>
-
-        </nav>
+    <?php include_once("nav.php"); ?>
 
 
     </div>
