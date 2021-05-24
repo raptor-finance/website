76812ae17d687cd57749ddaa545cff38e831f48f
MadCaper <59817666+MadCaper@users.noreply.github.com>
2021-05-23 23:41:29 -0300

    Lottery page working, v0.5
    Changed file type, added global nav inclusion, working lottery, tested for buying tickets, and a demo draw, set to draw weekly

--- new-website/lottery.php
+++ new-website/lottery.php
@@ -7,8 +7,8 @@
     <link rel="icon"
           type="image/png"
           href="/images/lsolo-logo.svg">
-    <link rel="stylesheet" href="src/app.css">
-    <link rel="stylesheet" href="src/media.css">
+    <link rel="stylesheet" href="https://raptr.finance/new-website/src/app.css">
+    <link rel="stylesheet" href="https://raptr.finance/new-website/src/media.css">
 </head>
 <body>
 
@@ -19,56 +19,15 @@
     <div class="navigation-wrapper">
 
         <div class="logo-wrapper">
-            <img src="./images/logo.svg" class="img-logo">
+            <img src="https://raptr.finance/new-website/images/logo.svg" class="img-logo">
             <button class="navbar-toggler" type="button" data-target="#left-navbar"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="iconify" data-icon="bi:list" data-inline="false"></span>
             </button>
         </div>
-        <nav id="left-navbar">
-            <ul>
-                <li>
-                    <a href="index.html" class="nav-item-home">Home</a>
-                </li>
-                <li>
-                    <a href="about.html" class="nav-item-roadmap">About</a>
-                </li>
-                <li>
-                    <a href="https://raptr.finance/staking" class="nav-item-staking">Staking</a>
-                </li>
-                <!--<li>
-                    <a href="lottery.html" class="nav-item-lottery active">Lottery</a>
-                </li>-->
-                <li>
-                    <a href="faq.html" class="nav-item-faq">FAQ</a>
-                </li>
-<!--                <li>
-                    <a href="news.html" class="nav-item-news">News</a>
-                </li>-->
-            </ul>
 
-            <div class="navigation-footer">
-                <div class="text-center text-lg-left mb-2">
-                    <a href="https://twitter.com/raptor_token" class="btn-social">
-                        <span class="iconify" data-icon="jam:twitter-circle" data-inline="false"></span>
-                    </a>
-                    <a href="https://t.me/PhilosoRaptorToken" class="btn-social">
-                        <span class="iconify" data-icon="ri:telegram-line" data-inline="false"></span>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xf9A3FdA781c94942760860fc731c24301c83830A" class="btn btn-success btn-block">
-                        Buy on PancakeSwap
-                    </a>
-                </div>
-                <div class="mt-3">
-                    <a href="https://bscscan.com/token/0xf9a3fda781c94942760860fc731c24301c83830a#balances" class="btn btn-light btn-block">
-                        View on BSCscan
-                    </a>
-                </div>
-            </div>
-
-        </nav>
+    <?php include_once("nav.php"); ?>
+    
     </div>
 
     <div class="content-wrapper">
@@ -77,7 +36,7 @@
                 <div class="row">
                     <div class="col-12 col-lg-10">
 
-                        <img src="./images/lottery.svg" class="mb-3" width="250">
+                        <img src="https://raptr.finance/new-website/images/lottery.svg" class="mb-3" width="250">
                         <div>
                             <p class="text-white">
                                 This is a simple, non-custodial Proof of Work Random Number Generation <br>
@@ -115,7 +74,7 @@
 
                                             <div class="mt-3">
                                                 <div>
-                                                    <a onclick="buyTicket()"  class="btn btn-dark btn-block">Purchase a ticket</a>
+                                                    <a onclick="buyTicket()" class="btn btn-dark btn-block">Purchase a ticket</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -138,7 +97,7 @@
                                             </div>
                                             <div class="mt-3 mb-4">
                                                 <strong class="text-dark">Total tickets for this round</strong>
-                                                <div id="totalticketslabel" class="text-dark-light">kjuydbf dhhf</div>
+                                                <div id="totalticketslabel" class="text-dark-light">An amount</div>
                                             </div>
 
                                         </div>
@@ -155,15 +114,16 @@
 </main>
 
 <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-<script src="/web3.min.js"></script>
-<script src="/bootstrap.min.js"></script>
+<script src="https://raptr.finance/new-website/jquery.min.js"></script>
+<script src="https://raptr.finance/new-website//web3.min.js"></script>
+<script src="https://raptr.finance/new-website/bootstrap.min.js"></script>
 <script>
   function showTicketsBalance(amount) {
     document.getElementById("ticketslabel").innerHTML = new Intl.NumberFormat().format(amount);
   }
 
   function showTokenBalance(amount) {
-    document.getElementById("balancelabel").innerHTML = new Intl.NumberFormat().format(amount/(10**9));
+    document.getElementById("balancelabel").innerHTML = new Intl.NumberFormat().format(amount/(10**9))
   }
 
   function showJackpot(amount) {
@@ -187,7 +147,7 @@ async function getLastWinner() {
 }
 
   async function showTotalTickets() {
-	document.getElementById("totalticketslabel").innerHTML = (await getTotalTickets());
+	document.getElementById("totalticketslabel").innerHTML = (await getTotalTickets())
   }
 
 
@@ -207,7 +167,7 @@ async function getLastWinner() {
 		purchaseTicket();
 	}
     else {
-        alert("Insufficient RAPTR balance!")
+        alert("Insufficient RAPTOR balance!")
     }
   }
 
