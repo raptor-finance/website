1ca83e6703bbf0774330a14792532eeef47312dc
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2021-05-25 17:59:36 +0200

    added FAQ

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,7 +25,10 @@ const pagesInNavigator = [
 	{ id: 'news', title: 'News', component: StaticHtmlComponent, componentProps: {src: '<h3 class="coming-soon">Coming soon</h3>'} },
 	{ id: 'staking', title: 'Staking', component: StakingComponent },
 	{ id: 'lottery', title: 'Lottery', component: LotteryComponent },
-	{ id: 'faq', title: 'FAQ', component: StaticHtmlComponent, componentProps: {src: '<h3 class="coming-soon">Coming soon</h3>'}  },
+	{ id: 'faq', title: 'FAQ', component: StaticHtmlComponent, componentProps: {src: [
+				require('./content/faq.css'),
+				require('./content/faq.html')]
+	} },
 ]
 
 // initialize modals
