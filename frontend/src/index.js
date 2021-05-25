347ee776159158c5f52114c4b0df4daf59c418a5
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2021-05-25 18:00:00 +0200

    Hidden news for now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,7 +22,7 @@ const pagesInNavigator = [
 		require('./content/about.css'),
 		require('./content/about.html')]
 	} },
-	{ id: 'news', title: 'News', component: StaticHtmlComponent, componentProps: {src: '<h3 class="coming-soon">Coming soon</h3>'} },
+	//{ id: 'news', title: 'News', component: StaticHtmlComponent, componentProps: {src: '<h3 class="coming-soon">Coming soon</h3>'} },
 	{ id: 'staking', title: 'Staking', component: StakingComponent },
 	{ id: 'lottery', title: 'Lottery', component: LotteryComponent },
 	{ id: 'faq', title: 'FAQ', component: StaticHtmlComponent, componentProps: {src: [
