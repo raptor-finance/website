11134f72304ac7788255792a21ce556c4feaec8b
ygboucherk <ygboucherk@gmail.com>
2021-07-16 01:21:19 +0200

    Update farmComponent.tsx

--- src/components/pages/farmComponent.tsx
+++ src/components/pages/farmComponent.tsx
@@ -2,11 +2,24 @@ import * as React from 'react';
 
 import { withTranslation, WithTranslation, TFunction } from 'react-i18next';
 import { BaseComponent } from '../shellInterfaces';
+import { RaptorLottery } from '../contracts/lottery';
+
 
 import './farmComponent.css';
 
 export type FarmProps = {};
-export type FarmState = {};
+export type FarmState = {
+	farm?: RaptorFarm,
+	wallet?: Wallet,
+	looping?: boolean,
+
+	address?: string,
+	balance?: number,
+	lpbalance?: number,
+	stakedlp?: number,
+  rewards?: number,
+}
+};
 
 class FarmComponent extends BaseComponent<FarmProps & WithTranslation, FarmState> {
 
