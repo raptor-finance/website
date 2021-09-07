f2b9c3072b3c7c402e00aff7c6c3b1d845c78fb9
ygboucherk <ygboucherk@gmail.com>
2021-09-07 07:04:50 +0200

    Migration stuff

--- src/components/contracts/raptor.ts
+++ src/components/contracts/raptor.ts
@@ -4,11 +4,13 @@ import {Contract} from 'web3-eth-contract';
 import * as web3 from 'web3-utils';
 
 export const RaptorAddress = "0xf9A3FdA781c94942760860fc731c24301c83830A";
+export const RaptorAddressv3 = "0x44C99Ca267C2b2646cEEc72e898273085aB87ca5";
 export const DonationWalletAddress = "0x50dF6f99c75Aeb6739CB69135ABc6dA77C588f93"
 
 export class Raptor {
 	private readonly _wallet: Wallet;
 	private readonly _contract: Contract;
+	private readonly _contractv3: Contract;
 
 	private _balance: number = 0;
 	private _stake: number = 0;
@@ -27,6 +29,10 @@ export class Raptor {
 
 	get contract(): Contract {
 		return this._contract;
+	}
+
+	get contractv3(): Contract {
+		return this._contractv3;
 	}
 
 	get wallet(): Wallet {
@@ -65,6 +71,15 @@ export class Raptor {
 	async claim(): Promise<void> {
 		await this._contract.methods.claimStakingRewards().send({'from': this._wallet.currentAddress});
 		await this.refresh();
+	}
+	
+	async migrate(amount:number): Promise<void> {
+		if (this._balance >= _amount) {
+			await.this._contract.methods.approveAndCall(this._contractv3._address,web3.toWei(String(amount),'gwei'),"0x0");
+		}
+		else {
+			throw `Your balance isn't sufficient to migrate this amount, maximum : ${this._balance}`;
+		}
 	}
 
 	async refresh(): Promise<void> {
