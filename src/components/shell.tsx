e1ed877f79b28e392cf7f971444fbae636108422
monstar09 <62696158+monstar09@users.noreply.github.com>
2021-09-28 21:05:29 +0300

    website revamp

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -34,18 +34,11 @@ export class Shell extends BaseComponent<ShellProps, ShellState> {
 				<ShellNav pages={pages} />
 				<div className="content-wrapper">
 					<div className="listing-banner d-flex align-items-center shadow">
-<<<<<<< Updated upstream
-			    	<FontAwesomeIcon icon={faInfoCircle} className="info-icon" />
-			        	<h1>The RaptorSwap <strong>BETA</strong> is <strong>finally here</strong>! We are looking for projects to list on our exchange. Reach out to us on <a href="https://t.me/RaptorSwap">Telegram</a><br/>
-					    Please migrate your Raptor V2 tokens to Raptor V3 as soon as possible.<br/></h1>
-			</div>
-=======
 						<FontAwesomeIcon icon={faInfoCircle} className="info-icon" />
 						<h1>The RaptorSwap <strong>BETA</strong> is <strong>finally here</strong>! We are looking for projects to list on our exchange. Reach out to us on <a href="https://t.me/RaptorSwap">Telegram</a>.<br/>
 						<b>IMPORTANT NOTE:</b> We've applied 100% Fee on every Buy/Sell order on Raptor V2. Please <b><u><a href="../migrate">migrate</a></u></b> to Raptor V3 as soon as possible to continue the Buy/Sell orders</h1>
 
 					</div>
->>>>>>> Stashed changes
 					<Switch>
 						{pages.map(page => (
 							<Route key={`${page.id}`} path={'/' + page.id}>
