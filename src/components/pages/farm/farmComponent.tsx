6082dc84a9f9767f2731f376ebfbef2862849760
ygboucherk <ygboucherk@gmail.com>
2021-10-03 10:32:08 +0200

    Ah yes tvl

--- src/components/pages/farm/farmComponent.tsx
+++ src/components/pages/farm/farmComponent.tsx
@@ -375,7 +375,6 @@ class FarmComponent extends BaseComponent<FarmProps & WithTranslation, FarmState
                 </AnimatedNumber>
               </h2>
             </div>
-			{/*
 			<div className="d-flex justify-content-between tvl">
 			  <h2>TVL: </h2>
 			  <h2>
@@ -388,7 +387,6 @@ class FarmComponent extends BaseComponent<FarmProps & WithTranslation, FarmState
                 </AnimatedNumber>
 			  </h2>
 			</div>
-			*/}
             <div className="d-flex justify-content-between pool">
               <h2>Liquidity Pool: </h2>
               <h2><u>{liquidityPool}</u></h2>
