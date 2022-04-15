c1795139e96a4215ff6397699e404d339fe70bf0
ygboucherk <ygboucherk@gmail.com>
2022-04-15 21:39:48 +0200

    Create chain.js

---
+++ src/components/contracts/chain.js
@@ -0,0 +1,64 @@
+import {Wallet} from '../wallet';
+import {Contract} from 'web3-eth-contract';
+// import { ethers } from 'ethers';
+import * as web3 from 'web3-utils';
+
+
+class RaptorTestnetInterface {
+	private readonly wallet: Wallet;
+	private readonly node: string;
+	
+	
+	constructor(walletInstance: Wallet, nodeURL: string) {
+		this.wallet = walletInstance;
+		this.node = nodeURL;
+	}
+	
+	convertFromHex(hex) {
+		var hex = hex.toString();//force conversion
+		var str = '';
+		for (var i = 0; i < hex.length; i += 2)
+			str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+		return str;
+	}
+
+	convertToHex(str) {
+		var hex = '';
+		for(var i=0;i<str.length;i++) {
+			hex += ''+str.charCodeAt(i).toString(16);
+		}
+		return hex;
+	}
+	
+	async getCurrentEpoch() {
+		return (await (await fetch(`${this.node}/chain/getlastblock`)).json()).result.miningData.proof;
+	}
+	
+	async getAccountInfo(account) {
+		return (await (await fetch(`${this.node}/accounts/accountInfo/${account}`)).json()).result;
+	}
+
+	async getHeadTx(account) {
+		let accountInfo = (await getAccountInfo(account));
+		return accountInfo.transactions[accountInfo.transactions.length-1];
+	}
+
+	async buildLegacyTransferTx(to, tokens) {
+		const parent = (await getHeadTx(this.wallet.currentAddress));
+		let data = {"from":this.wallet.currentAddress, "to":web3.toChecksumAddress(to), "tokens":tokens, "parent": parent, "epoch": (await this.getCurrentEpoch()),"type": 0};
+		let strdata = JSON.stringify(data);
+		const hash = web3.soliditySha3(strdata);
+		const signature = (await this.wallet.sign(strdata));
+		const tx = {"data": data, "sig": signature, "hash": hash, "nodeSigs": {}};
+		return this.convertToHex(JSON.stringify(tx));
+	}
+
+	async sendTransaction(signedTx) {
+		console.log(signedTx);
+		return (await (await fetch(`${this.node}/send/rawtransaction/?tx=${signedTx}`)).json()).result;
+	}
+	
+	getVrs(sig) {
+		return (('0x' + sig.substring(2).substring(128, 130)), ('0x' + sig.substring(2).substring(0, 64)), ('0x' + sig.substring(2).substring(64, 128)))
+	}
+}
