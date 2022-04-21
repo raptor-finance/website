c0c13a923697b41876787c804a748946a1a1ad4c
ygboucherk <ygboucherk@gmail.com>
2022-04-21 15:20:18 +0200

    UI updates

--- src/components/pages/crosschain/crosschainComponent.tsx
+++ src/components/pages/crosschain/crosschainComponent.tsx
@@ -42,7 +42,8 @@ class CrossChainComponent extends BaseComponent<CrossChainProps & withTranslatio
 		this.deposit = this.deposit.bind(this);
 		this.withdraw = this.withdraw.bind(this);
 		this.handleAmountUpdate = this.handleAmountUpdate.bind(this);
-		this.setMaxAmount = this.setMaxAmount.bind(this);
+		this.setMaxDepositAmount = this.setMaxDepositAmount.bind(this);
+		this.setMaxWithdrawalAmount = this.setMaxWithdrawalAmount.bind(this);
 		this.state = {};
 	}
 	
@@ -142,10 +143,14 @@ class CrossChainComponent extends BaseComponent<CrossChainProps & withTranslatio
 		this.updateState({ ctValue:tokens });
 	}
 	
-	setMaxAmount() {
+	setMaxDepositAmount() {
 		const state = this.readState();
-		let tokens = state.balance;
-		this.updateState({ ctValue:valueIn});
+		this.updateState({ ctValue: ((!!state.raptor) ? state.raptor.balancev3 : 0) });
+	}
+	
+	setMaxWithdrawalAmount() {
+		const state = this.readState();
+		this.updateState({ ctValue: ((!!state.chain) ? state.chain.balance : 0) });
 	}
 	
 	async deposit() {
@@ -195,7 +200,7 @@ class CrossChainComponent extends BaseComponent<CrossChainProps & withTranslatio
 				<div className="row text-white staking-header">
 					<div className="col-md-12">
 							<div className="migration-title">
-							<b><font size="6"><span>Raptor</span><span style={{ color: "#31c461" }}> Chain</span></font></b>
+							<b><font size="6"><span>Raptor</span><span style={{ color: "#31c461" }}>Chain</span></font></b>
 							{state.address ?
 								(<a className="shadow btn btn-primary ladda-button btn-md btn-wallet float-right" disabled={state.pending} role="button" onClick={this.disconnectWallet}>
 									{state.pending && <span className="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"> </span>}
@@ -217,29 +222,34 @@ class CrossChainComponent extends BaseComponent<CrossChainProps & withTranslatio
 						<div className="shadow d-flex flex-column flex-fill gradient-card primary">
 							<h2>{t('migration.wallet.wallet_address')}</h2>
 							<p>{state.address || t('migration.wallet.connect_wallet')}</p>
-							<h2>BSC-side balance</h2>
-							<AnimatedNumber
-								value={numeral(tokenBalance || 0).format('0.00')}
-								duration="1000"
-								formatValue={value => `${Number(parseFloat(value).toFixed(2)).toLocaleString('en', { minimumFractionDigits: 2 })}`}
-								className="staking-info"
-							>
-								0 Raptor
-							</AnimatedNumber>
+							<h2>Balance breakdown</h2>
+							<div onClick={this.setMaxDepositAmount}>
+								<AnimatedNumber
+									value={numeral(tokenBalance || 0).format('0.00')}
+									duration="1000"
+									formatValue={value => `BSC-side : ${Number(parseFloat(value).toFixed(2)).toLocaleString('en', { minimumFractionDigits: 2 })}`}
+									// className="staking-info"
+									onclick={this.setMaxDepositAmount}
+								>
+									0 Raptor
+								</AnimatedNumber>
+							</div>
+							<div onClick={this.setMaxWithdrawalAmount}>
+								<AnimatedNumber
+									value={numeral(coinBalance || 0).format('0.00')}
+									duration="1000"
+									formatValue={value => `RaptorChain-side : ${Number(parseFloat(value)).toLocaleString('en')}`}
+									// className="staking-info"
+									onclick={this.setMaxWithdrawalAmount}
+								>
+									0 Raptor
+								</AnimatedNumber>
+							</div>
 							<p>Enter the amount that you want to transfer:</p>
 							<div>		
-								<input className="input-amount" placeholder="Enter an amount..." onChange={this.handleAmountUpdate} value={state.ctValue}></input><button className="btns-migrate" id="btn-max" onClick={this.setMaxAmount}>Max</button>
+								<input className="input-amount" placeholder="Enter an amount..." onChange={this.handleAmountUpdate} value={state.ctValue}></input>
                             </div>
 							<br/>
-							<h2>RaptorChain-side balance</h2>
-							<AnimatedNumber
-								value={numeral(coinBalance || 0).format('0.00')}
-								duration="1000"
-								formatValue={value => `${Number(parseFloat(value)).toLocaleString('en')}`}
-								className="staking-info"
-							>
-								0 Raptor
-							</AnimatedNumber>
                             <div className="d-flex justify-content-center button-row">
 					         	<button id="btn-deposit" className="btn btn-primary btn-md link-dark align-self-center stake-confirm" onClick={this.deposit}>Deposit</button>
 					         	<button id="btn-deposit" className="btn btn-primary btn-md link-dark align-self-center stake-confirm" onClick={this.withdraw}>Withdraw</button>
