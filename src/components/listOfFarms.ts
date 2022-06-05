c94ac6cb45a14ff18403dd36ffce44021b0965c4
ygboucherk <ygboucherk@gmail.com>
2022-06-05 18:02:02 +0200

    Added RPTR/bscDUCO farm

--- src/components/listOfFarms.ts
+++ src/components/listOfFarms.ts
@@ -10,4 +10,14 @@ export const farmsList = [
 	"addLiquidityURL": "https://pancakeswap.finance/add/BNB/0x44C99Ca267C2b2646cEEc72e898273085aB87ca5",
 	"version":1
   },
+  {
+    "pid": 2,
+    "pairName": "RPTR-bscDUCO",
+    "logo": "images/farm/bscduco-rptr.png",
+    "fees": "NO FEES",
+    "liquidityPool": "Pancake",
+    "enableGlow": true,
+	"addLiquidityURL": "https://pancakeswap.finance/add/0xCF572cA0AB84d8Ce1652b175e930292E2320785b/0x44C99Ca267C2b2646cEEc72e898273085aB87ca5",
+	"version":1
+  },
 ]
