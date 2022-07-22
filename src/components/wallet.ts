7c565d5dd1ac4ba42cbe2c6ff4001d805680c582
ygboucherk <ygboucherk@gmail.com>
2022-07-22 15:38:16 +0200

    Added chainID

--- src/components/wallet.ts
+++ src/components/wallet.ts
@@ -64,7 +64,7 @@ export class Wallet {
 
 		const provider: any = this._provider;
 		if (provider) {
-			if (((provider.chainId != 56) && (provider.networkVersion != 56)) && ((provider.chainId != this._raptorChainID) && (provider.networkVersion != this._raptorChainID)) && ((provider.chainId != 97) && (provider.networkVersion != 97))) {
+			if (((provider.chainId != 1380996178) && (provider.networkVersion != 1380996178)) && ((provider.chainId != 56) && (provider.networkVersion != 56)) && ((provider.chainId != this._raptorChainID) && (provider.networkVersion != this._raptorChainID)) && ((provider.chainId != 97) && (provider.networkVersion != 97))) {
 				if (provider.isMetaMask) {
 					const networkinfo = [{
 						chainId: '0x38',
