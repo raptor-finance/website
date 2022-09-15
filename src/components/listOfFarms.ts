da3febd0416dea153829fca35e6fbd99fcaa3f9b
ygboucherk <ygboucherk@gmail.com>
2022-09-15 23:23:37 +0200

    Add to list of farms (frontend only)
    Not added to contracts yet

--- src/components/listOfFarms.ts
+++ src/components/listOfFarms.ts
@@ -20,4 +20,14 @@ export const farmsList = [
 	"addLiquidityURL": "https://pancakeswap.finance/add/0xCF572cA0AB84d8Ce1652b175e930292E2320785b/0x44C99Ca267C2b2646cEEc72e898273085aB87ca5",
 	"version":1
   },
+  {
+    "pid": 1,
+    "pairName": "RPTR-BUSD",
+    "logo": "images/farm/busd-raptor.png",
+    "fees": "NO FEES",
+    "liquidityPool": "Pancake",
+    "enableGlow": true,
+	"addLiquidityURL": "https://pancakeswap.finance/add/0x44C99Ca267C2b2646cEEc72e898273085aB87ca5/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+	"version":1
+  },
 ]
