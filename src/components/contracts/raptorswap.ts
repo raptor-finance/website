0309e68d99e14881b8b97a46d775c370c4965e25
ygboucherk <ygboucherk@gmail.com>
2022-10-01 10:27:37 +0200

    Adding raptorswap base

--- src/components/contracts/raptorswap.ts
+++ src/components/contracts/raptorswap.ts
@@ -2,6 +2,8 @@ import {Wallet} from '../wallet';
 import {Contract} from 'web3-eth-contract';
 import * as web3 from 'web3-utils';
 
+export const FactoryAddress = "0xB8F7aAdaC20Cd74237dDAB7AC7ead317BF049Fa3";
+export const RouterAddress = "0x397D194abF71094247057642003EaCd463b7931f";
 
 export class RaptorSwap {
 	private readonly _wallet: Wallet;
@@ -9,5 +11,6 @@ export class RaptorSwap {
 
 	constructor(wallet: Wallet, router) {
 		this._wallet = wallet;
+		t
 	}
 }
