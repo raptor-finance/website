7ab5546c3bcc0f98a98509918262e741846d5863
ygboucherk <ygboucherk@gmail.com>
2022-10-19 15:38:08 +0200

    Update raptorswap.ts

--- src/components/contracts/raptorswap.ts
+++ src/components/contracts/raptorswap.ts
@@ -6,6 +6,8 @@ export const FactoryAddress = "0xB8F7aAdaC20Cd74237dDAB7AC7ead317BF049Fa3";
 export const RouterAddress = "0x397D194abF71094247057642003EaCd463b7931f";
 export const WRPTRAddress = "0xeF7cADE66695f4cD8a535f7916fBF659936818C4";
 
+export const EVM_MAX_UINT256 = "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+
 export class RaptorSwap {
 	private readonly _wallet: Wallet;
 	private readonly _router: Contract;
@@ -40,6 +42,14 @@ export class RaptorSwap {
 		return (tokenA == "RPTR")?0: ((tokenB == "RPTR")? 1: 2);
 	}
 	
+	async ensureApproval(tokenAddr, rawAmount) {
+		const _token = this._wallet.connectToContract(tokenAddr, require("./erc20.abi.json"));
+		const _currentAllowance = _token.methods.allowance(this._wallet.currentAddress, RouterAddress);
+		if (rawAmount > _currentAllowance) {
+			_token.methods.approve(RouterAddress, EVM_MAX_UINT256).send({"from": this._wallet.currentAddress});
+		}
+	}
+	
 	async getAmountOut(amountIn, path) {
 		const amts = await this._router.methods.getAmountsOut(amountIn, path).call();
 		return amts[amts.length-1];
@@ -58,7 +68,7 @@ export class RaptorSwap {
 	}
 
 	async swapRPTRToToken(amountIn, path) {
-		return (await this._router.methods.swapExactETHForTokens(0, path, this._wallet.currentAddress, "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({"from": this._wallet.currentAddress, "value": amountIn, "gas": 500000}))
+		return (await this._router.methods.swapExactETHForTokens(0, path, this._wallet.currentAddress, EVM_MAX_UINT256).send({"from": this._wallet.currentAddress, "value": amountIn, "gas": 500000}));
 	}
 	
 	async swap(_amountIn, assetFrom, assetTo) {
