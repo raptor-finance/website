d4f929b9856f78f3edfae2c5eee661e4612983a0
ygboucherk <ygboucherk@gmail.com>
2022-10-23 00:01:51 +0200

    Update back-end

--- src/components/contracts/raptorswap.ts
+++ src/components/contracts/raptorswap.ts
@@ -8,19 +8,73 @@ export const WRPTRAddress = "0xeF7cADE66695f4cD8a535f7916fBF659936818C4";
 
 export const EVM_MAX_UINT256 = "115792089237316195423570985008687907853269984665640564039457584007913129639935"
 
+export class LiquidityPair {
+	private readonly _wallet: Wallet;
+	private readonly _router: Contract;
+	private readonly _factory: Contract;
+	private readonly _lp: Contract;
+	
+	token0: string; // token addresses
+	token1: string;
+	
+	reserve0: BigInt; // total reserves
+	reserve1: BigInt;
+	
+	balance0: BigInt; // user's pooled balances
+	balance1: BigInt;
+	
+	totalSupply: BigInt; // total LP's supply
+	lpbalance: BigInt; // user's LP balance
+	
+	setupPromise: Promise;
+	
+	
+	constructor(wallet: Wallet, router: Contract, factory: Contract, lpAddr: string) {
+		this._wallet = wallet;
+		this._router = router;
+		this._factory = factory;
+		this._lp = this._wallet.connectToContract(lpAddr, require("./lptoken.abi.json"));
+		this.setupPromise = this.setupStuff();
+	}
+	
+	async setupStuff() {
+		this.token0 = await this._lp.methods.token0().call();
+		this.token1 = await this._lp.methods.token1().call();
+		this.refresh();
+	}
+	
+	async refresh() {
+		const _r = await this._lp.methods.getReserves().call();
+		this.reserve0 = BigInt(_r._reserve0);
+		this.reserve1 = BigInt(_r._reserve1);
+		this.totalSupply = BigInt(await this._lp.methods.totalSupply().call());
+		
+		this.lpbalance = BigInt(await this._lp.methods.balanceOf(this._wallet.currentAddress).call());
+		
+		this.balance0 = ((this.reserve0 * this.lpbalance) / this.totalSupply);
+		this.balance1 = ((this.reserve1 * this.lpbalance) / this.totalSupply);
+	}
+}
+
 export class RaptorSwap {
 	private readonly _wallet: Wallet;
 	private readonly _router: Contract;
 	private readonly _factory: Contract;
+	private _pairs: any;
 
 	constructor(wallet: Wallet) {
 		this._wallet = wallet;
-		this._factory = wallet.connectToContract(FactoryAddress, require('./swapfactory.abi.json'))
-		this._router = wallet.connectToContract(RouterAddress, require('./swaprouter.abi.json'))
+		this._factory = wallet.connectToContract(FactoryAddress, require('./swapfactory.abi.json'));
+		this._router = wallet.connectToContract(RouterAddress, require('./swaprouter.abi.json'));
+		this.fetchPairs();
 	}
 	
 	calcName(tokenName) {
 		return ((tokenName == "RPTR") ? WRPTRAddress : tokenName);
+	}
+	
+	get pairs() {
+		return _pairs;
 	}
 	
 	getPath(tokenA, tokenB) {
@@ -42,11 +96,31 @@ export class RaptorSwap {
 		return (tokenA == "RPTR")?0: ((tokenB == "RPTR")? 1: 2);
 	}
 	
+	getTokenAt(tokenAddr) {
+		return this._wallet.connectToContract(tokenAddr, require("./erc20.abi.json"));
+	}
+	
+	getLpAt(lpAddr) {
+		return (new LiquidityPair(this._wallet, this._router, this._factory, lpAddr));
+	}
+	
+	async fetchPairs() {
+		let _p = [];
+		const _l = await this._factory.methods.allPairsLength().call();
+		for (let i = 0; i < _l; i++) {
+			let _pAddr = await this._factory.methods.allPairs(i).call()
+			_p.push(this.getLpAt(_pAddr));
+		}
+		this._pairs = _p;
+		console.log(this._pairs);
+	}
+	
 	async ensureApproval(tokenAddr, rawAmount) {
-		const _token = this._wallet.connectToContract(tokenAddr, require("./erc20.abi.json"));
-		const _currentAllowance = _token.methods.allowance(this._wallet.currentAddress, RouterAddress);
+		const _token = this.getTokenAt(tokenAddr);
+		const _currentAllowance = await _token.methods.allowance(this._wallet.currentAddress, RouterAddress).call();
+		console.log(_currentAllowance);
 		if (rawAmount > _currentAllowance) {
-			_token.methods.approve(RouterAddress, EVM_MAX_UINT256).send({"from": this._wallet.currentAddress});
+			await _token.methods.approve(RouterAddress, EVM_MAX_UINT256).send({"from": this._wallet.currentAddress});
 		}
 	}
 	
@@ -66,6 +140,8 @@ export class RaptorSwap {
 	
 	async swapTokenToRPTR(amountIn, path) {
 		await this.ensureApproval(path[0], amountIn);
+		console.log(path);
+		console.log(await this._router.methods.swapExactTokensForETH(amountIn, 0, path, this._wallet.currentAddress, EVM_MAX_UINT256).call({"from": this._wallet.currentAddress}))
 		return (await this._router.methods.swapExactTokensForETH(amountIn, 0, path, this._wallet.currentAddress, EVM_MAX_UINT256).send({"from": this._wallet.currentAddress, "gas": 500000}));
 	}
 
@@ -100,6 +176,16 @@ export class RaptorSwap {
 			case 2:
 				await this.swapTokenToToken(amountIn, _path);
 				break;
+		}
+	}
+	
+	async assetBalance(assetName) {
+		console.log(`Pulling ${assetName} balance`);
+		switch (assetName) {
+			case "RPTR":
+				return web3.fromWei(await this._wallet.eth_getBalance(this._wallet.currentAddress, EVM_MAX_UINT256));
+			default:
+				return web3.fromWei(await this.getTokenAt(assetName).methods.balanceOf(this._wallet.currentAddress).call());
 		}
 	}
 	
